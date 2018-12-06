cad1bb7b090290c759ea136538f8f6cbfaca33e0
Fgyter <fgyterr@yandex.ru>
2018-12-06 14:35:53 +0300

    To correct indention

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -1,51 +1,51 @@
 class LettersController < ApplicationController
 	before_action :set_letter, only: [:show, :edit, :update, :destroy]
 
-  def index
-    @letters = Letter.all
-  end
+	def index
+		@letters = Letter.all
+	end
 
-  def show
-  end
+	def show
+	end
 
-  def new
-    @letter = Letter.new
-  end
+	def new
+		@letter = Letter.new
+	end
 
-  def edit
-  end
+	def edit
+	end
 
-  def create
-    @letter = Letter.new(letter_params)
-    if @letter.save
-      redirect_to @letter
-    else
-      render :new
-    end
-  end
+	def create
+		@letter = Letter.new(letter_params)
+		if @letter.save
+			redirect_to @letter
+		else
+			render :new
+		end
+	end
 
-  def update
-    if @letter.update(letter_params)
-      redirect_to @letter
-    else
-      render :edit
-    end
-  end
+	def update
+		if @letter.update(letter_params)
+			redirect_to @letter
+		else
+			render :edit
+		end
+	end
 
-  def destroy
-    if @letter.destroy
-      redirect_to letters_path
-    end
-  end
+	def destroy
+		if @letter.destroy
+			redirect_to letters_path
+		end
+	end
 
-  private
+	private
 
-  def set_letter
-    @letter = Letter.find(params[:id])
-    redirect_to letters_path if @letter.nil?
-  end
+	def set_letter
+		@letter = Letter.find(params[:id])
+		redirect_to letters_path if @letter.nil?
+	end
 
-  def letter_params
-    params.require(:letter).permit(:url_address, :email, :comment, :user_id)
-  end
+	def letter_params
+		params.require(:letter).permit(:url_address, :email, :comment, :user_id)
+	end
 end
